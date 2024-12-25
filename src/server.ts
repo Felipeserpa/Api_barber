a433f0bc61007fffb3b6865307163bcafe633f83
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-12-25 12:45:24 -0300

    implementaçao middleware de erro e o cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(router);
+//implementando cors
+app.use(cors());
+
+//implementando o middleware de erro
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
 app.listen(3333, () => {
   console.log("serviço online");
