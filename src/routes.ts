4f22ef2e263aec4f1d681ff40e27ee0441f16f4e
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-12-26 04:16:55 -0300

    implementaçao da pagina de login

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,18 @@
 import { Router, Request, Response } from "express";
+
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
-//router.get("/teste", (req: Request, res: Response) => {
-//return res.json({ ok: "true" });
-//});
-
 // --Rotas User --
-
+//rota de cadastro
 router.post("/users", (req: Request, res: Response) => {
   return new CreateUserController().handle(req, res);
 });
+//rota de login
+router.post("/session", (req: Request, res: Response) => {
+  return new AuthUserController().handle(req, res);
+});
+
 export { router };
